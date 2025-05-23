fc4422339e5330bf9293bd4edf13f8bb6212e705
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-05-23 08:44:07 +0300

    add code to the backend

---
+++ backend/models/User.js
@@ -0,0 +1,43 @@
+import { EntitySchema } from "typeorm";
+// import TemplatePage from "./TemplatePage.js";
+
+const User = new EntitySchema({
+  name: "User",
+  tableName: "users",
+  columns: {
+    id: {
+      primary: true,
+      type: "int",
+      generated: true,
+    },
+    firstname: {
+      type: "varchar",
+      unique: true,
+    },
+    lastname: {
+      type: "varchar",
+      unique: true,
+    },
+    email: {
+      type: "varchar",
+      unique: true,
+    },
+    password: {
+      type: "varchar",
+    },
+    image: {
+      type: "varchar",
+      nullable: true,
+    },
+  },
+//   relations: {
+//     TemplatePage: {
+//       type: "one-to-many",
+//       target: "TemplatePage",
+//       inverseSide: "user",
+//       cascade: true,
+//     },
+//   },
+});
+
+export default User;
