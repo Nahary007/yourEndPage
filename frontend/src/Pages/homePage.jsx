aebb68fe586946247042e7add5f473ebdb01ab31
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-05-26 12:04:15 +0300

    configure header for the homePage finished

--- frontend/src/Pages/homePage.jsx
+++ frontend/src/Pages/homePage.jsx
@@ -1,18 +1,25 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import Header2 from "../Components/header2";
+import Hero from "../Components/Hero";
+import Features from "../Components/Features";
+import Showcase from "../Components/ShowCase";
+import HowItWorks from "../Components/HowItWorks";
+import Footer from "../Components/Footer";
+
 export default function HomePage() {
 
-    const navigate = useNavigate();
-    const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/');
-  };
+  //   const navigate = useNavigate();
+  //   const handleLogout = () => {
+  //   localStorage.removeItem('token');
+  //   navigate('/');
+  // };
     return(
     <>
-    <button
-      onClick={handleLogout}
-      className="p-4 rounded-xl bg-violet-500 text-black shadow-md border border-red-600 transition-all duration-300 hover:bg-red-600 hover:shadow-lg hover:scale-105 focus:outline-none focus:ring-2 focus:ring-red-400 active:scale-95"
-    >
-      Deconnecter
-    </button></>
+        <Header2 />
+        <Hero />
+        <Features />
+        <Showcase />
+        <HowItWorks />
+        <Footer />
+    </>
 );
 }
